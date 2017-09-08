d34666a8e45f29ae288af3c8f86ef53a344abcee
MORILLE Jérôme <morille.jerome@groupagrica.com>
2017-09-08 17:11:18 +0200

    packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const del = require('del');
+const  gzip = require('gulp-gzip');
+
 const gulp = require('gulp');
 const polymerBuildUtils = require('gulp-polymer-build-utils');
 const runSequence = require('run-sequence');
@@ -6,10 +8,14 @@ const yargs = require('yargs');
 
 const argv = polymerBuildUtils.addYargs(yargs).help().argv;
 
-gulp.task('dist:clean', () => del('dist'));
+gulp.task('clean', ['build:clean', 'dist:clean' ]);
+gulp.task('dist:clean', () => del(['dist' ]));
+gulp.task('build:clean', () => del(['build' ]));
+
 gulp.task('dist:prepare', ['dist:clean'], () => {
     const config = require('./polymer.json');
-    return polymerBuildUtils.build(config)()
+    console.log(config);
+    return polymerBuildUtils.polymerBuild(config)
         .pipe(gulp.dest('dist'));
 });
 gulp.task('default', cb => {
@@ -17,4 +23,10 @@ gulp.task('default', cb => {
 });
 gulp.task('dist:test', function() {
     return polymerBuildUtils.runWct(argv);
+});
+
+gulp.task('gzip', function () {
+    return gulp.src(['build/**/*'])
+        .pipe(gzip())
+        .pipe(gulp.dest('dist/gzip'));
 });
